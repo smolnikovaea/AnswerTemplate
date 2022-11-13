bd4a12ee878df19738baddd92ea475567294a929
smolnikovaea <115584462+smolnikovaea@users.noreply.github.com>
2022-11-13 23:59:37 +0300

    Delete Task1.py

--- Task1.py
+++
@@ -1,6 +0,0 @@
-# TODO решить с помощью list comprehension и распечатать его
-
-slovar = [{'bin': bin(number), 'dec': number, 'hex': hex(number), 'oct': oct(number)} for number in range(16)]
-from pprint import pprint
-
-pprint(slovar)
